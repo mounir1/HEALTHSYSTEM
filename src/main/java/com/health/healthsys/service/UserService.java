2e4a1f8b128df2edf75a9cb2820ae5493cd6fd51
bulag <bernaulag@gmail.com>
2022-05-18 21:53:33 +0300

    fix method

--- src/main/java/com/health/healthsys/service/UserService.java
+++ src/main/java/com/health/healthsys/service/UserService.java
@@ -1,22 +1,29 @@
 package com.health.healthsys.service;
 
 import com.health.healthsys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
+    @Autowired(required = false)
     private UserRepository mUserRepository;
 
-    public UserService(UserRepository aUserRepository) {
-        mUserRepository = aUserRepository;
-    }
+//    public UserService() {
+//    }
+//
+//    public UserService(UserRepository aUserRepository) {
+//        mUserRepository = aUserRepository;
+//    }
 
     public UserRepository getUserRepository() {
         return mUserRepository;
     }
 
-    public String getUser() {
-        return getUserRepository().getUser();
+    public List<String> getUser() {
+        return getUserRepository().getUserList();
     }
 }
